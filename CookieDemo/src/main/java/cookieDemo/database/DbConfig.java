465b949f58fb6fcb928489508015e194fd094cfb
Vikas Sharmma <vs_416@yahoo.com>
2023-07-11 15:28:10 +0530

    Remove unnecessary files

--- CookieDemo/src/main/java/cookieDemo/database/DbConfig.java
+++
@@ -1,8 +0,0 @@
-package cookieDemo.database;
-
-public interface DbConfig {
-	String Driver = "com.mysql.cj.jdbc.Driver";
-	String connectionString = "jdbc:mysql://localhost:3306/serveletdatabase";
-	String username = "root";
-	String password = "";
-}
